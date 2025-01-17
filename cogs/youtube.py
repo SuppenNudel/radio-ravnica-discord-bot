9f1625e6e79114f5f09618cb574919f4d73a59c9
Cedric Rohm <5153475+SuppenNudel@users.noreply.github.com>
2025-01-17 08:28:31 +0100

    fixed stream checking

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -31,44 +31,47 @@ class Youtube(commands.Cog):
                 target_list.pop(0)  # Remove the oldest item to make space
             target_list.append(item)
 
-    async def handle_video(self, discord_channel:discord.TextChannel, channel_name, video, content_type):
+    def get_list(self, content_type, channel_name):
+        if content_type == "video":
+            return self.videos[channel_name]
+        if content_type == "stream":
+            return self.streams[channel_name]
+        raise Exception(f"Invalid content type {content_type}")
+
+    async def handle_video(self, loop, discord_channel:discord.TextChannel, channel_name, video, content_type):
         video_id = video['videoId']
 
-        if self.check.current_loop == 0:
-            self.add_unique_with_limit(video_id, self.videos[channel_name])
+        video_list = self.get_list(content_type, channel_name)
+
+        if loop.current_loop == 0:
+            self.add_unique_with_limit(video_id, video_list)
             return
         
-        in_list = video_id in self.videos[channel_name]
+        in_list = video_id in video_list
         has_badges = 'badges' in video
         if has_badges:
             return
-        if not in_list and not has_badges:
+        if not in_list:
             url = f"https://youtu.be/{video_id}"
             if content_type == "video":
                 await discord_channel.send(f"<@&{os.getenv('ROLE_ANNOUNCEMENT')}>\nneues Video von **{channel_name}**\n\n{url}")
-                self.add_unique_with_limit(video_id, self.videos[channel_name])
             elif content_type == "stream":
                 await discord_channel.send(f"<@&{os.getenv('ROLE_ANNOUNCEMENT')}>\n{channel_name} streamt gleich! Kommt, schaut vorbei!\n\n{url}")
-        # return video_id
+            self.add_unique_with_limit(video_id, video_list)
 
     @tasks.loop(seconds=60)
     async def check_livestream(self):
+        discord_channel = self.bot.get_channel(self.channel_id)
+        if not type(discord_channel) == discord.TextChannel:
+            log.error(f"type of discord_channel is not discord.TextChannel, but {type(discord_channel)}")
+            return
+        
         for channel_name in self.channels:
-            streams = scrapetube.get_channel(channel_url=self.channels[channel_name], limit=2, content_type="streams")
+            streams = scrapetube.get_channel(channel_url=self.channels[channel_name], limit=3, content_type="streams")
             stream_list = list(streams)[::-1]
-            for stream in stream_list:
-                video_id = stream['videoId']
-                if self.check_livestream.current_loop == 0:
-                    self.add_unique_with_limit(video_id, self.streams[channel_name])
-                    continue
-                if video_id in self.streams[channel_name]:
-                    continue
-                log.info(f"Posting Stream with id {video_id}")
-                discord_channel = self.bot.get_channel(self.channel_id)
-                if not type(discord_channel) == discord.TextChannel:
-                    log.error(f"type of discord_channel is not discord.TextChannel, but {type(discord_channel)}")
-                    continue
-                await discord_channel.send(f"<@&{os.getenv('ROLE_ANNOUNCEMENT')}>\n{channel_name} streamt gleich! Kommt, schaut vorbei!\n\n{f"https://youtu.be/{video_id}"}")
+            
+            for video in stream_list:
+                await self.handle_video(self.check, discord_channel, channel_name, video, "stream")
 
     @tasks.loop(seconds=60*5)
     async def check(self):
@@ -81,10 +84,10 @@ class Youtube(commands.Cog):
             videos = scrapetube.get_channel(channel_url=self.channels[channel_name], limit=5)
 
             video_list = list(videos)[::-1]
-
+            
             # post videos that have not been saved on self.videos yet
             for video in video_list:
-                await self.handle_video(discord_channel, channel_name, video, "video")
+                await self.handle_video(self.check_livestream, discord_channel, channel_name, video, "video")
 
 def setup(bot:Bot):
     bot.add_cog(Youtube(bot))
