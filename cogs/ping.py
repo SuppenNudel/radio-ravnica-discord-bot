81bc29e893a96cb7a8275d9fb4ee4965bda63886
Cedric Rohm <5153475+SuppenNudel@users.noreply.github.com>
2025-01-27 17:52:34 +0100

    remind me now as slash command

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,6 +1,6 @@
-from ezcord import Cog, log
-from discord.ext.commands import slash_command
-from discord import ApplicationContext, Bot
+from ezcord import log, Cog
+from discord.ext.commands import slash_command, has_role, has_permissions
+from discord import ApplicationContext, Bot, default_permissions, InteractionContextType, IntegrationType
 
 class Ping(Cog):
     def __init__(self, bot:Bot):
@@ -10,9 +10,22 @@ class Ping(Cog):
     async def on_ready(self):
         log.debug(self.__class__.__name__ + " is ready")
 
-    @slash_command(description="Ping the bot")
+    # Weitere Checks https://gist.github.com/Painezor/eb2519022cd2c907b56624105f94b190
+    @slash_command(
+        description="Ping the bot",
+        integration_type={IntegrationType.user_install},
+        contexts={InteractionContextType.bot_dm} #, InteractionContextType.private_channel
+    )
+    @has_permissions(manage_guild=True)
+    # @has_role("Moderator") 
     async def ping(self, ctx:ApplicationContext):
         await ctx.send_response("Pong", ephemeral="True")
+
+    @Cog.listener()
+    async def on_application_command_error(self, ctx:ApplicationContext, error):
+        log.error(error)
+        await ctx.respond(f"Es ist ein Fehler aufgetreten: ```{error}```", ephemeral=True)
+        raise error
 
 
 def setup(bot:Bot):
--- cogs/remind_me.py
+++ cogs/remind_me.py
@@ -1,20 +1,18 @@
 from discord.ext import commands, tasks
 from discord import Bot, Message
-from discord.commands import message_command
+from discord.commands import message_command, slash_command
 from ezcord.emb import EzContext
 from ezcord import log
 import discord
 from datetime import datetime
-import arrow
 import dateparser
 import modules.notion as notion
 import os
 import pytz
-import locale
 from discord.ui import Modal
+from discord.utils import format_dt
 
 timezone = pytz.timezone("Europe/Berlin")
-# locale.setlocale(locale.LC_TIME, "de")
 
 DB_ID_REMIND_ME = os.getenv("DATABASE_ID_REMIND_ME")
 DB_FIELD_DATE = "Timestamp"
@@ -24,75 +22,126 @@ DB_FIELD_CHANNEL = "Channel"
 DB_FIELD_REASON = "Reason"
 DB_FIELD_GUILD = "Guild"
 
-def save_reminder_request(user, referenced_message, channel_id, date, reason, guild_id):
+def save_reminder_request(user, date, reason, guild_id, channel_id, message_id):
     user_id = user.id
-    payload = (notion.NotionPayloadBuilder()
-        .add_title(DB_FIELD_MESSAGE, str(referenced_message))
+    payload_builder = (notion.NotionPayloadBuilder()
         .add_date(DB_FIELD_DATE, start=date)
         .add_text(DB_FIELD_CHANNEL, str(channel_id))
         .add_text(DB_FIELD_USER, str(user_id))
-        .add_text(DB_FIELD_REASON, reason)
         .add_text(DB_FIELD_GUILD, str(guild_id))
-    ).build()
+    )
+    if message_id:
+        payload_builder.add_title(DB_FIELD_MESSAGE, str(message_id))
+    if reason:
+        payload_builder.add_text(DB_FIELD_REASON, reason)
+    payload = payload_builder.build()
     notion.add_to_database(DB_ID_REMIND_ME, payload)
 
-class MyModal(Modal):
-    def __init__(self, user, message_id=None, channel_id=None, guild_id=None, *args, **kwargs):
-        super().__init__(
-            *args,
-            **kwargs
-        )
+async def handle_input(interaction: discord.Interaction|EzContext, followup_message, time_input, reason, user:discord.member.Member|discord.User, message=None):
+    if not time_input:
+        await interaction.followup.edit_message(followup_message.id, content=r"Ohne Angaben kann ich nichts machen ¯\_(ツ)_/¯")
+        return
+    
+    parsed_date = dateparser.parse(time_input, settings={'RETURN_AS_TIMEZONE_AWARE': True})
+    if not parsed_date:
+        raise ValueError(f"Ich konnte deine Zeitangabe nicht interpretieren: {time_input}")
+    if parsed_date < datetime.now(tz=timezone):
+        raise ValueError(f"Interpretierter Zeitpunkt: {format_dt(parsed_date, style='R')}\nIch kann dich nicht in der Vergangenheit erinnern. Zeitreisen wurden noch nicht erfunden.")
+
+    if message:
+        guild_id = message.guild.id
+        channel_id = message.channel.id
+        message_id = message.id
+    else:
+        guild_id = interaction.guild.id
+        channel_id = interaction.channel.id
+        message_id = None
+    
+    save_reminder_request(user, parsed_date, reason, guild_id, channel_id, message_id)
+    await interaction.followup.edit_message(followup_message.id, content=f"✅ {user.mention}, ich werde dich {format_dt(parsed_date, style='R')} erinnern (am {format_dt(parsed_date, style='F')})")
+
+class ReminderModal(Modal):
+    def __init__(self, user, message:Message, time_input=None, reason=None):
+        super().__init__(title = "Erstellung einer Erinnerung")
 
         self.add_item(discord.ui.InputText(
-            label="Wann soll ich dich erinnern? In...",
-            placeholder="2 stunden - 1 minute - 5 jahre 2 monate 2 minuten"
+            label="Wann soll ich dich erinnern?",
+            placeholder="z.B. in 10 Minuten, morgen um 17 Uhr, am 24.12.2025 um 12:00 Uhr",
+            required=True,
+            value=f"{time_input} <- bitte anpassen" if time_input else None,
         ))
         self.add_item(discord.ui.InputText(
             label="Grund",
             placeholder="Eine Gedächtnisstütze, warum du erinnert werden wolltest",
             required=False,
+            style=discord.InputTextStyle.long,
+            value=reason,
         ))
 
         self.user:discord.Member = user
-        self.channel_id = channel_id
-        self.message_id = message_id
-        self.guild_id = guild_id
+        self.message = message
     
     async def callback(self, interaction: discord.Interaction):
-        user_input = self.children[0].value
-        if user_input:
-            parsed_date = dateparser.parse(user_input)
-            if parsed_date:
-                arrow_object = arrow.get(parsed_date)
-                relative_time = arrow_object.humanize(locale="de")
-            else:
-                log.error(f"Error parsing date '{user_input}'")
-                # TODO handle error
-                return
+        await interaction.response.defer(ephemeral=True)
+        followup_message  = await interaction.followup.send(
+            "⌛ Ich arbeite an deiner Anfrage, das kann kurz dauern...", ephemeral=True
+        )
+        if followup_message is None:
+            log.error("followup_message is None")
+            return
+        time_input = self.children[0].value
+        reason = self.children[1].value
 
-            reason = self.children[1].value
-            save_reminder_request(self.user, self.message_id, self.channel_id, parsed_date, reason, self.guild_id)
+        try:
+            await handle_input(interaction, followup_message, time_input, reason, self.user, message=self.message)
+        except Exception as e:
+            await interaction.followup.edit_message(followup_message.id, content=f"❌ Fehler\n{str(e)}\nKlicke auf den Button unten, um es erneut zu versuchen.", view=ReopenModalView(self.user, self.message, time_input, reason))
 
-            await interaction.response.send_message(f"{self.user.mention}, ich werde dich in {relative_time} erinnern (am {parsed_date.strftime('%A, %d. %B %Y')} um {parsed_date.strftime('%H:%M:%S')})", ephemeral=True)
-        else:
-            await interaction.response.send_message(r"Ohne Angaben kann ich nichts machen ¯\_(ツ)_/¯", ephemeral=True)
+class ReopenModalButton(discord.ui.Button):
+    def __init__(self, user, message, time_input=None, reason=None):
+        super().__init__(label="Erneut versuchen", style=discord.ButtonStyle.primary)
+        self.user = user
+        self.message = message
+        self.time_input = time_input
+        self.reason = reason
+
+    async def callback(self, interaction: discord.Interaction):
+        modal = ReminderModal(self.user, self.message, self.time_input, self.reason)
+        await interaction.response.send_modal(modal)
+
+class ReopenModalView(discord.ui.View):
+    def __init__(self, user, message, time_input=None, reason=None):
+        super().__init__()
+        self.add_item(ReopenModalButton(user, message, time_input, reason))
 
 class RemindMe(commands.Cog):
     def __init__(self, bot:Bot):
         self.bot = bot
-        self.guild = os.getenv("GUILD")
-        if not self.guild:
-            raise Exception("GUILD env var is not defined")
+        
+    @slash_command(description="Erstelle eine Erinnerung")
+    async def erinnere_mich(self, ctx:EzContext, wann, grund):
+        await ctx.response.defer(ephemeral=True)
+        followup_message  = await ctx.followup.send(
+            "⌛ Working on your request, this might take a moment...", ephemeral=True
+        )
+        reason = grund
+        if followup_message is None:
+            log.error("followup_message is None")
+            return
+        try:
+            await handle_input(ctx, followup_message, wann, reason, ctx.author)
+        except Exception as e:
+            await ctx.followup.edit_message(followup_message.id, content=f"❌ Fehler:\n{str(e)}\nBitte versuche es noch einmal!")
 
-    @message_command(name="Erinnere mich in/am ...")
-    async def remind_me(self, ctx:EzContext, message:Message):
+    @message_command(name="Erstelle Erinnerung...")
+    async def remind_me(self, ctx:EzContext, message:Message):        
         # get message id
         # save user that used the interaction
         # save to database
-        modal = MyModal(title = "Erstellung einer Erinnerung", user=ctx.author, message_id=message.id, channel_id=message.channel.id, guild_id=self.guild)
+        modal = ReminderModal(user=ctx.author, message=message)
         await ctx.send_modal(modal)
 
-    async def send_reminder_message(self, message_id, channel_id, user_id, reason=None):
+    async def send_reminder_message(self, guild, message_id, channel_id, user_id, reason=None):
         bot = self.bot
         try:
             # Fetch the channel
@@ -106,8 +155,16 @@ class RemindMe(commands.Cog):
             
             log.debug(f"got channel: {channel.name}")
 
-            # Fetch the message by its ID
-            message = await channel.fetch_message(message_id)
+            description = None
+            timestamp = None
+            author = None
+            message = None
+            if message_id:
+                # Fetch the message by its ID
+                message = await channel.fetch_message(message_id)
+                description = message.content if message.content else "No content"
+                timestamp = message.created_at
+                author = message.author
 
             # Fetch the user by ID
             user = await bot.fetch_user(user_id)
@@ -115,41 +172,41 @@ class RemindMe(commands.Cog):
             # Create an embed to forward the message
             embed = discord.Embed(
                 # title="Link zur Nachricht",
-                description=message.content if message.content else "No content",
+                description=description,
                 color=discord.Color.blurple(),  # You can change the embed color
                 # url=message.jump_url,
-                timestamp=message.created_at
+                timestamp=timestamp
             )
-            author = message.author
-            avatar_url = None
-            if hasattr(author, "guild_avatar"):
-                if author.guild_avatar:
-                    avatar_url = author.guild_avatar.url
-            if not avatar_url:
-                if hasattr(author, "avatar"):
-                    if author.avatar:
-                        avatar_url = author.avatar.url
-            
-            embed.set_author(name=f"von {message.author.name}", icon_url=avatar_url)#, url=message.author.jump_url)
-            guild_icon_url = message.guild.icon.url if message.guild.icon else None
-            embed.set_footer(text=message.guild.name, icon_url=guild_icon_url)
+            if author:
+                avatar_url = None
+                if hasattr(author, "guild_avatar"):
+                    if author.guild_avatar:
+                        avatar_url = author.guild_avatar.url
+                if not avatar_url:
+                    if hasattr(author, "avatar"):
+                        if author.avatar:
+                            avatar_url = author.avatar.url
+                embed.set_author(name=f"von {message.author.name}", icon_url=avatar_url)#, url=message.author.jump_url)
 
-            content = f"Hey, du wolltest an diese Nachricht erinnert werden {message.jump_url}"
+            guild_icon_url = guild.icon.url if guild.icon else None
+            embed.set_footer(text=guild.name, icon_url=guild_icon_url)
+
+            content = "Hey, du wolltest "+ (f"an diese Nachricht erinnert werden {message.jump_url}" if message else "erinnert werden")
             if reason:
                 content += f"\nGrund: {reason}"
-                
-            # Attachments
-            if message.attachments:
-                for attachment in message.attachments:
-                    file = await attachment.to_file()
-                    embed.set_image(url=attachment.url)  # If the attachment is an image
-                    # Optionally, you can also send the file with the embed if you want
-                    await user.send(
-                        content=content,
-                        embed=embed,
-                        file=file
-                    )
-                return
+            
+            if message:
+                # Attachments
+                if message.attachments:
+                    for attachment in message.attachments:
+                        file = await attachment.to_file()
+                        embed.set_image(url=attachment.url)  # If the attachment is an image
+                        # Optionally, you can also send the file with the embed if you want
+                        await user.send(
+                            content=content,
+                            embed=embed,
+                            file=file
+                        )
 
             # Send the embed to the user via DM
             await user.send(content=content, embed=embed)
@@ -170,26 +227,27 @@ class RemindMe(commands.Cog):
 
     @tasks.loop(minutes=5)
     async def check_reminders(self):
-        filter_date = datetime.now(tz=timezone)
-        filter = (notion.NotionFilterBuilder()
-                  .add_date_filter(property_name=DB_FIELD_DATE, value=filter_date.strftime("%Y-%m-%d %H:%M"), condition=notion.DateCondition.ON_OR_BEFORE)
-                  .add_text_filter(property_name=DB_FIELD_GUILD, value=self.guild, condition=notion.TextCondition.EQUALS)
-                  .build())
-        entries = notion.get_all_entries(DB_ID_REMIND_ME, filter=filter)
-        for entry in entries:
-            my_entry = notion.Entry(entry)
-            timestamp = my_entry.get_date_property(DB_FIELD_DATE)
-            if timestamp['start'] > filter_date:
-                # noch zu früh
-                continue
-            user = my_entry.get_text_property(DB_FIELD_USER)
-            message = my_entry.get_text_property(DB_FIELD_MESSAGE)
-            channel = my_entry.get_text_property(DB_FIELD_CHANNEL)
-            reason = my_entry.get_text_property(DB_FIELD_REASON)
-            success = await self.send_reminder_message(message, channel, user, reason=reason)
-            if success:
-                # remove from database
-                notion.remove_entry(my_entry)
+        for guild in self.bot.guilds:
+            filter_date = datetime.now(tz=timezone)
+            filter = (notion.NotionFilterBuilder()
+                    .add_date_filter(property_name=DB_FIELD_DATE, value=filter_date.strftime("%Y-%m-%d %H:%M"), condition=notion.DateCondition.ON_OR_BEFORE)
+                    .add_text_filter(property_name=DB_FIELD_GUILD, value=str(guild.id), condition=notion.TextCondition.EQUALS)
+                    .build())
+            entries = notion.get_all_entries(DB_ID_REMIND_ME, filter=filter)
+            for entry in entries:
+                my_entry = notion.Entry(entry)
+                timestamp = my_entry.get_date_property(DB_FIELD_DATE)
+                if timestamp['start'] > filter_date:
+                    # noch zu früh
+                    continue
+                user = my_entry.get_text_property(DB_FIELD_USER)
+                message = my_entry.get_text_property(DB_FIELD_MESSAGE)
+                channel = my_entry.get_text_property(DB_FIELD_CHANNEL)
+                reason = my_entry.get_text_property(DB_FIELD_REASON)
+                success = await self.send_reminder_message(guild, message, channel, user, reason=reason)
+                if success:
+                    # remove from database
+                    notion.remove_entry(my_entry)
 
 def setup(bot:Bot):
     bot.add_cog(RemindMe(bot))
