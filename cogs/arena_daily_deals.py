1de20ce3e5aa6f7b9737fc1ee94970f5e12901da
Cedric Rohm <5153475+SuppenNudel@users.noreply.github.com>
2025-02-07 16:15:14 +0100

    Update arena_daily_deals.py
    Fix

--- cogs/arena_daily_deals.py
+++ cogs/arena_daily_deals.py
@@ -32,9 +32,7 @@ class ArenaDailyDeals(Cog):
     @discord.ext.tasks.loop(minutes=15)
     async def check_bsky_posts(self):
         latest_post = await self.get_latest_post()
-        if self.check_bsky_posts.current_loop == 0:
-            self.latest_post = latest_post
-        elif latest_post.uri == self.latest_post.uri:
+        if self.latest_post and not latest_post.uri == self.latest_post.uri:
             rkey = latest_post.uri.split("/")[-1]
             profile_identifier = BSKY_ARENA_DAILY_DEALS_HANDLE or self.author_did  # Use handle if available
             post_url = f"https://bsky.app/profile/{profile_identifier}/post/{rkey}"
@@ -42,6 +40,7 @@ class ArenaDailyDeals(Cog):
             
             channel:discord.TextChannel = await discord.utils.get_or_fetch(self.bot, "channel", CHANNEL_ID_ARENA)
             await channel.send(post_url)
+        self.latest_post = latest_post
 
 def setup(bot:Bot):
     bot.add_cog(ArenaDailyDeals(bot))
